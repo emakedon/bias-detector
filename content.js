30512223ede5c906c2e9fe5c54defa929d482a61
emakedon <emakedon1@gmail.com>
2022-02-22 11:22:32 -0600

    V2 is working  by highlighting paragraphs that contain key words

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 let isPressed = false;
+const wordarr = ["and"];
+// , "than", "even", "now", "say", "could", "can"];
 function gotMessage(message,sender,sendresponse)
 {
 	if (!isPressed){
@@ -7,12 +9,19 @@ function gotMessage(message,sender,sendresponse)
         let paragraphs = document.getElementsByTagName("p");
         for(elt of paragraphs)
         {
-            elt.style['background-color'] = '#ffdf65';
+            innerwords = elt.innerText
+            for (word of wordarr){
+                if (innerwords.includes(word)){
+                    console.log(innerwords)
+                    elt.style['background-color'] = '#ffdf65';
+                }
+            }
         }
+        
         toggleHighlight()
     }
     else{
-        console.log(message.clickedOff);
+        // console.log(message.clickedOff);
         let paragraphs = document.getElementsByTagName("p");
         for(elt of paragraphs)
         {
--- display.js
+++ display.js
@@ -1,10 +1,16 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message,sender,sendresponse)
-{
-	console.log(message.txt);
-	let paragraphs = document.getElementsByTagName("p");
-	for(elt of paragraphs)
-	{
-		elt.style['background-color'] = '#ffdf65';
-	}
-}
+// // const words = ["was", "is", "am", "and", "so", "or", "might", "more", "than", "even", "now", "say", "could", "can"];
+
+// chrome.runtime.onMessage.addListener(gotMessage);
+// function gotMessage(message,sender,sendresponse)
+// {
+// 	console.log(message.txt);
+// 	let paragraphs = document.getElementsByTagName("p");
+// 	for(elt of paragraphs)
+// 	{
+// 		if (elt.text in words){
+//             console.log("word is in paragraph")
+//             elt.style['background-color'] = '#ffdf65';
+//         }
+        
+// 	}
+// }
